089bcfbee7e3f7d7e2250b8b1ab7d8c6397ead98
Luis Martimor <luismartimor@gmail.com>
2021-11-10 20:12:36 +0100

    index.html actualizado 3

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 export const getGifs = async( category ) => {
 
-    const url = `https:api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=8Meq3jUIGZVvMtvBv6RYtLDq47T8urLk`
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=8Meq3jUIGZVvMtvBv6RYtLDq47T8urLk`
     const resp = await fetch( url )
     const { data } = await resp.json()
 
